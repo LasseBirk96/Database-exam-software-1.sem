cf5c8574090c6a1744d27565afebc822bb554b20
Lasse Birk <lassebirk@Lasses-MacBook-Pro.local>
2022-05-09 14:45:25 +0200

    Now able to retrieve data, needs tunning though

--- hbase/main.py
+++ hbase/main.py
@@ -31,5 +31,13 @@ def populate_table(table):
     batcher.send()
 
 
-table = create_table
-populate_table(table)
+    table = create_table()
+    populate_table(table)
+
+connection = connect()
+table = connection.table("products")
+
+rows = table.rows([b"product"])
+for key, data in rows:
+    print(data) #This one prints all that data
+    print(data[b'product_id:ffd635b7-828f-467b-abea-e719c4347eb4']) #This prints the dictionary with this specific id
