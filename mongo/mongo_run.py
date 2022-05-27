877ac83483b998b98b63cc6e8cc5afc244749b56
daniel poulsen <daniel.poulsen@live.dk>
2022-05-27 15:42:47 +0200

    mongo create order from redis products

--- mongo/mongo_run.py
+++ mongo/mongo_run.py
@@ -1,3 +1,4 @@
+import requests
 import sys
 
 sys.path.append("mongo")
@@ -6,12 +7,12 @@ from flask import Flask, request, jsonify, session
 from flask_restful import Resource, Api, reqparse
 from pymongo import MongoClient
 from entities.Product import Product
-from database.mongo_commands import insert_order, get_orders, boot_db, delete_order
+from database.mongo_commands import insert_order, get_orders, boot_db, delete_order, update_order
 from utility import user_jwt
 
 from __main__ import app
-
 app.secret_key = "super secret key"
+
 # laver en decorator istedet da det gør det lidt mere reusable
 def requires_user(f):
     @wraps(f)
@@ -48,24 +49,54 @@ def mongohome():
 @requires_user
 def create_order():
     user_id = session.get("user_id")
-    data = request.get_json()
-    p = Product(
-        data["product_id"],
-        data["product_name"],
-        data["product_brand"],
-        data["item_number"],
-        data["color"],
-        data["grill_type"],
-        data["amount_of_burners"],
-        data["bread_basket"],
-        data["amount_of_wheels"],
-        data["length_in_cm"],
-        data["width_in_cm"],
-        data["product_price"],
-    )
-    insert_order(boot_db(), user_id, p.return_product())
+    product_list = [
+        "1",
+        "2"
+    ]
+
+    # api_endpoint = "TBD"
+    # r = requests.post(url = api_endpoint, data = product_list)
+    # response = r.json
+    list_of_products = []
+    for products in product_list:
+        product = thisdemonstratestheapicalltoredisgigascuffednametho(products)
+        list_of_products.append(product)
+
+    insert_order(boot_db(), user_id, list_of_products)
     return "Persisted order successfully"
 
+def thisdemonstratestheapicalltoredisgigascuffednametho(x):
+    if x == "1":
+         return {
+            "product_id": "123", 
+            "product_name": "Grill",
+            "product_brand": "Weber", 
+            "item_number" : "1",
+            "color" : "Black",
+            "grill_type" : "Gas",
+            "amount_of_burners" : "6",
+            "bread_basket" : "Yes",
+            "amount_of_wheels" : "6",
+            "length_in_cm" : "120",
+            "width_in_cm" : "80",
+            "product_price": "9000"
+            }
+    
+    if x == "2":
+         return {
+            "product_id": "12345", 
+            "product_name": "Grill",
+            "product_brand": "Weber", 
+            "item_number" : "1",
+            "color" : "Black",
+            "grill_type" : "Gas",
+            "amount_of_burners" : "6",
+            "bread_basket" : "Yes",
+            "amount_of_wheels" : "6",
+            "length_in_cm" : "120",
+            "width_in_cm" : "80",
+            "product_price": "9000"
+            }
 
 # GET ORDERS BY USER ID
 @app.route("/mongo/order", methods=["GET"])
@@ -78,3 +109,11 @@ def get():
 @app.route("/mongo/order/delete/<id>", methods=["DELETE"])
 def delete(id):
     return "Returned: " + delete_order(boot_db(), str(id))
+
+# Updates order by order id
+# @app.route("/mongo/order/update/<id>", methods=["UPDATE"])
+# def update(id):
+#     data = request.get_json()
+#     item = data[""]
+#     new_item = data[""]
+#     return "Returned: " + update_order(boot_db(), str(id), item, new_item)
